a5a12b34af2fab5aaf76e1a673dd7d6efde0c5d7
skript023 <fatur_muhammad@bk.ru>
2022-09-30 17:51:23 +0700

    Fix Unload Dll

---
+++ scarlet-nexus/src/hooks/process_event.cpp
@@ -0,0 +1,17 @@
+#include "hooking.hpp"
+#include "script_mgr.hpp"
+
+namespace big
+{
+	void APIENTRY hooks::process_event(UObject* _this, UFunction* function, void* parms)
+	{
+		if (g_running)
+		{
+			if (function->get_name() == "ReceiveTick" || function->get_name() == "Received_NotifyTick")
+			{
+				
+			}
+		}
+		return g_hooking->m_process_event_hook.get_original<decltype(&process_event)>()(_this, function, parms);
+	}
+}
