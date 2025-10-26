f2a9bb24a8614f7baf42e4a973cd63ad40f8f132
Vladimir Leonov <fatur_muhammad@bk.ru>
2025-10-26 23:49:53 +0700

    remove home rendering

--- src/menu/core/home.cpp
+++ src/menu/core/home.cpp
@@ -4,11 +4,11 @@ namespace big
 {
 	void view::home()
 	{
-        canvas::add_submenu<regular_submenu>("Home", SubmenuHome, [](regular_submenu* sub)
+        /*canvas::add_submenu<regular_submenu>("Home", SubmenuHome, [](regular_submenu* sub)
         {
             sub->add_option<sub_option>("Player", nullptr, SubmenuPlayer);
             sub->add_option<sub_option>("Stats", nullptr, SubmenuStats);
             sub->add_option<sub_option>("Settings", nullptr, SubmenuSettings);
-        });
+        });*/
 	}
 }
